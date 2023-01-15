934dfac70485af5d2501ab5fd9ad132ac2e5cc9d
shoorik04 <shoorikshoorik2004@gmail.com>
2023-01-15 22:35:21 +0400

    shemogevlos chemi tavi

---
+++ app/src/main/java/com/example/finaluri_gamocda/ChangePasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.finaluri_gamocda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
